ca26fd4f603568618eb2a7a6eafee383029f4bea
David Poole <davidpoole@Davids-MacBook-Pro.local>
2020-05-16 21:15:17 -0400

    pep8 compliance

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # :mask: webloc_address_extractor
 
-Simple python3 script for extracting URLs from Safari .webloc (bookmark) files
+Simple python3 script for extracting URLs from Safari .webloc (bookmark) files in current folder or folders specified as command line arguments.
 
 *usage: webloc_address_extractor [folder 1] ... [folder n]*
 
--- webloc_address_extractor.py
+++ webloc_address_extractor.py
@@ -11,19 +11,22 @@ import plistlib
 
 comment_character = "# "
 
+
 def process_folder(folder_name):
     """Examine folder for .webloc files and print contained URLs to stdout"""
     for filename in glob.glob(folder_name + "/*.webloc"):
-        with open(filename,'rb') as plf:
+        with open(filename, 'rb') as plf:
             results = plistlib.load(plf)
-            print("# {filename}\n{address}\n".format(filename=filename,address=results['URL']))
+            print("# {filename}\n{address}\n".
+            format(filename=filename, address=results['URL']))
 
 
 def main():
     """testing"""
     for i, arg in enumerate(sys.argv):
-        if (i>0):
+        if (i > 0):
             process_folder(arg)
 
 if __name__ == '__main__':
     main()
+
